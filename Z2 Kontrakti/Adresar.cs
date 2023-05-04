9843c3999c106a50412da8648b9e554b9327fb46
Dimitrije <dosha1968@gmail.com>
2023-05-04 12:07:00 +0200

    Add project files.

---
+++ Z2 Kontrakti/Adresar.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Z2_Kontrakti
+{
+    internal class Adresar
+    {
+        private Dictionary<string, Kontakt> kontakti = new Dictionary<string, Kontakt>();
+        bool Dodaj(Kontakt k)
+        {
+            if(kontakti.ContainsKey(k.GlavniBroj))
+            {
+                return false;
+            }
+            else
+            {
+                kontakti.Add(k.GlavniBroj, k);
+            }
+            return true;
+        }
+        bool Dodaj(string ime, string prezime, string glavnibroj)
+        {
+            Kontakt ko = new Kontakt(ime, prezime, glavnibroj);
+            return(Dodaj(ko));
+        }
+        bool Blokiraj(string broj)
+        {
+            if(kontakti.ContainsKey(broj))
+            {
+                kontakti.Remove(broj);
+                return true;
+            }
+            return false;
+        }
+        void Share(string FileName, string glavniBroj)
+        {
+
+        }
+        void Receive(string FileName)
+        {
+
+        }
+        void Backup (string FileName)
+        {
+
+        }
+    }
+}
---
+++ Z2 Kontrakti/Kontakt.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Z2_Kontrakti
+{
+    internal class Kontakt
+    {
+        private string ime;
+
+        public string Ime
+        {
+            get { return ime; }
+            set { ime = value; }
+        }
+        private string prezime;
+
+        public string Prezime
+        {
+            get { return prezime; }
+            set { prezime = value; }
+        }
+        private string glavnibroj;
+
+        public string GlavniBroj
+        {
+            get { return glavnibroj; }
+            set { glavnibroj = value; }
+        }
+        private List<string> ostalibrojevi;
+
+        public List<string> Ostalibrojevi
+        {
+            get { return ostalibrojevi; }
+            set { ostalibrojevi = value; }
+        }
+
+        public Kontakt(string ime, string prezime, string glavnibroj)
+        {
+            this.ime = ime;
+            this.prezime = prezime;
+            this.glavnibroj = glavnibroj;
+        }
+
+        bool DodajBroj(string dodatniBroj)
+        {
+            if (dodatniBroj != glavnibroj)
+            {
+                foreach (string item in ostalibrojevi)
+                {
+                    if (dodatniBroj == item)
+                    {
+                        return false;
+                    }
+                }
+                ostalibrojevi.Add(dodatniBroj);
+            }
+            return false;
+        }
+        bool IzbrisBroj(string dodatniBroj)
+        {
+            foreach (string item in ostalibrojevi)
+            {
+                if (dodatniBroj == item)
+                {
+                    ostalibrojevi.Remove(item);
+                    return true;
+                }
+            }
+            return false;
+        }
+        public override string ToString()
+        {
+            string str = "";
+            str += ime;
+            str += ", ";
+            str += prezime;
+            str += ", ";
+            str += glavnibroj;
+            if (ostalibrojevi == null)
+            {
+                return str;
+            }
+            else
+            {
+                str += "[";
+                for (int i = 0; i < ostalibrojevi.Count - 1; i++)
+                {
+                    str += ostalibrojevi;
+                    str += ", ";
+                }
+                str += ostalibrojevi[ostalibrojevi.Count - 1];
+                str += "]";
+                return str;
+            }
+        }
+    }
+}
